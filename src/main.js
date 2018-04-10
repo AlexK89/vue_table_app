68d49657804aa63182ac27c8f5546058ef05a052
Vasile Cojusco <kozhushko.vasyl@gmail.com>
2018-04-10 14:20:40 +0100

    added stylings

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 new Vue({
-	el: '#table',
+	el: '#table-container',
 	data: {
 		sortKey: 'name',
 		reverse: false,
 		search: '',
-		columns: ['name', 'sku'],
+		columns: ['id', 'name', 'sku'],
 		dataArray: []
 	},
 	mounted() {
+		//AJAx request
 		axios
 			.get('./src/data.json')
 			.then(response => {
@@ -15,19 +16,23 @@ new Vue({
 			})
 	},
 	computed: {
-		orderedUsers: function () {
-			if(!this.reverse) {
+		// Sort items
+		orderedUsers() {
+			if (!this.reverse) {
 				return _.orderBy(this.dataArray, this.sortKey, 'asc')
 			}
 			return _.orderBy(this.dataArray, this.sortKey, 'desc')
 		},
-		filteredUsers: function () {
+
+		// Filter items
+		filteredUsers() {
 			return this.orderedUsers.filter((user) => {
 				return user.name.toLowerCase().indexOf(this.search.toLowerCase()) !== -1 || user.sku.indexOf(this.search) !== -1
 			})
 		}
 	},
 	methods: {
+		// Define sorting order
 		sortBy(sortKey) {
 			this.reverse = (this.sortKey == sortKey) ? !this.reverse : false;
 			this.sortKey = sortKey;
